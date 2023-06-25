426198fce194129817a25b4eb6598e112b7ca5a0
Naomi <naomiirider@gmail.com>
2023-06-25 21:38:26 +0100

    Add additional operators

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
 
-console.log('Enter your first number:');
+console.log('==========================');
+
+console.log('Please enter the operator: ');
+const op = readline.prompt();
+
+console.log('Please enter the first number: ');
 const firstnum = readline.prompt();
 
-console.log('Enter your second number:');
+console.log('Please enter the second number: ');
 const secondnum = readline.prompt();
 
-console.log(parseFloat(firstnum) * parseFloat(secondnum));
+if (op == '+'){
+    console.log('The answer is ' + (parseFloat(firstnum) + parseFloat(secondnum)));
+}
+else if (op == '-'){
+    console.log('The answer is ' + (parseFloat(firstnum) - parseFloat(secondnum)));
+}
+else if (op == '*'){
+    console.log('The answer is ' + (parseFloat(firstnum) * parseFloat(secondnum)));
+}
+else if (op == '/'){
+    console.log('The answer is ' + (parseFloat(firstnum) / parseFloat(secondnum)));
+}
