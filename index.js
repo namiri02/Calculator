5e8c40684022ff9df76db0fabf3797048f150da9
Naomi <naomiirider@gmail.com>
2023-07-02 11:43:40 +0100

    Add loops

--- index.js
+++ index.js
@@ -1,40 +1,71 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator!');
 
-console.log('==========================');
-
-console.log('Please enter the operator: ');
-const op = readline.prompt();
-
-console.log('How many numbers do you want to ' + op + '?');
-const quantity = readline.prompt();
-
-const arr = new Array(+quantity);
-
-for (let i = 0; i < quantity; i++){
-    console.log('Please enter number ' + (i+1) + ':');
-    arr[i] = parseFloat(readline.prompt());
-
-
+    console.log('==========================');
 }
 
-var total = 0;
-
-for (let i = 0; i < quantity; i++){
-    if (op == '+'){
-        total += arr[i];
-    }
-    else if (op == '-'){
-        total -= arr[i];
-    }
-    else if (op == '*'){
-        total *= arr[i];
-    }
-    else {
-        total /= arr[i];
-    }
+function inputString(message){
+    console.log(message);
+    return readline.prompt();
 }
 
+function inputNumber(message){
+    let response;
+    do {
+        response = +getStringInputWithPrompt(prompt);
+    } while (isNaN(response));
+    return response;
+}
 
-console.log('The answer is: ' + total);
+function inputOperator(){
+    return inputString('Please enter the operator:')
+}
+
+function getNumberArray(){
+    const quantity = inputNumber('How many numbers do you want to ' + op + '?');
+    
+    const arr = new Array(quantity);
+    
+    for (let i = 0; i < quantity; i++){
+        arr[i] = inputNumber('Please enter number ' + (i+1) + ':');
+    }
+
+    return arr;
+}
+
+function calculateAnswer(op, arr) {
+    var total = 0;
+    
+    for (let i = 0; i < quantity; i++){
+        if (op == '+'){
+            total += arr[i];
+        }
+        else if (op == '-'){
+            total -= arr[i];
+        }
+        else if (op == '*'){
+            total *= arr[i];
+        }
+        else {
+            total /= arr[i];
+        }
+    }
+
+    return total;
+}
+
+function performOneCalculation() {
+    const op = inputOperator();
+    const numarr = getNumberArray();
+    const answer = calculateAnswer(op, numarr);
+    
+    console.log('The answer is: ' + answer + "\n");
+}
+
+printWelcomeMessage();
+
+while (true) {
+    performOneCalculation();
+}
